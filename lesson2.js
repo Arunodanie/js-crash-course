c9a3fc684f572098e2e9684a81698027f57b948b
Arunodanie <bhagyaarunodanie@gmail.com>
2024-08-30 01:07:38 +0530

    learn strings

---
+++ lesson2.js
@@ -0,0 +1,18 @@
+console.log("This is lesson2.js file")
+//  string, numbers, boolean, null, undefined
+
+const myname = 'john';
+const age = 30;
+
+// concatenation
+console.log ('my name is ' + myname + ' and i am ' + age )
+// template string
+console.log(`my name is ${myname} and I am ${age}`)
+
+const hello =`my name is ${myname} and I am ${age}`;
+
+console.log(hello);
+
+ 
+
+
