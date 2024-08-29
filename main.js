4cbfd81152ee30f6d81f0932890a56172efdeddb
Arunodanie <bhagyaarunodanie@gmail.com>
2024-08-30 00:24:58 +0530

    print variable types

--- main.js
+++ main.js
@@ -7,9 +7,34 @@ const score = 10;
 
 console.log(score);
 
+const myName = 'jean';
+PrintVariableType(myName);
 
-const name = 'jean';
+const myEmptyString = "";
+PrintVariableType(myEmptyString);
+
 const age = 31;
+console.log(`Type of ${age}: ${typeof(age)}`);
+PrintVariableType(myEmptyString);
+
 const rating = 4.5;
+PrintVariableType(rating);
+
 const iscool = true;
-const x = null;
+PrintVariableType(iscool);
+
+const myNullValue = null;
+PrintVariableType(myNullValue);
+
+const myUndefinedValue = undefined;
+PrintVariableType(myUndefinedValue);
+
+const myObject = {id : "00125", firstName: "John", lastName: "Makeen", school: "NewYork highschool"}
+PrintVariableType(myObject);
+
+let myVariable;
+PrintVariableType(myVariable);
+
+function PrintVariableType(input){
+    console.log(`Type of ${input}: ${typeof(input)}`);
+}
